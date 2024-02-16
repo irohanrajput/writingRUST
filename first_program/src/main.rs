9ac2757dc8735a7f6707cbfeb995b474eb97a99a
irohanrajput <itusharsingh27@gmail.com>
2024-02-17 02:01:26 +0530

    hello Rust

---
+++ first_program/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!("yo, now we're rusting thingssss ");
+    let my_name = "Rohan";
+    println!("and... am I {} ???", my_name); 
+}
+
---
+++ first_program/target/debug/first_program.d
@@ -0,0 +1,1 @@
+/home/irohanrajput/Desktop/learn/writingRUST/first_program/target/debug/first_program: /home/irohanrajput/Desktop/learn/writingRUST/first_program/src/main.rs
